d0d08f6226734987b7620e002c4ddb03d7727c63
F3INT H34RTED <68115056+F3INTH34RTED@users.noreply.github.com>
2021-06-24 20:11:12 +0400

    Added CPU function

--- Exponentiation/Source.c
+++ Exponentiation/Source.c
@@ -8,10 +8,15 @@
 #include <CL/cl.h>
 #endif // __APPLE__
 
+#define ARRAY_SIZE 1024
+const unsigned int C_ARRAY_SIZE = ARRAY_SIZE;
+
 char *Get_Kernel_File(char *file_name);
+void Exponentiation_On_CPU(int array_size_percentage);
 
 int main()
 {
+    Exponentiation_On_CPU(50);
 	system("pause");
 	return 0;
 }
@@ -21,7 +26,7 @@ char *Get_Kernel_File(char *file_name)
     FILE *programFile;
     char *programSource;
     size_t program_source_size;
-    programFile = fopen(file_name, "rb"); /*  VERY IMPORTANT: "rb" on Windows */
+    programFile = fopen(file_name, "rb");
     if (!programFile)
         return NULL;
     fseek(programFile, 0, SEEK_END);
@@ -32,4 +37,10 @@ char *Get_Kernel_File(char *file_name)
     if (programSource != 0) fread(programSource, sizeof(char), program_source_size, programFile);
     fclose(programFile);
     return programSource;
+}
+
+void Exponentiation_On_CPU(int array_size_percentage)
+{
+    double a_s = array_size_percentage / 100.0;
+    printf("%lf\n", a_s);
 }
