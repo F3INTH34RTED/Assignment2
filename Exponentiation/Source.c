4c1ad102da203fa006e4888832d51da3a8e388b9
F3INT H34RTED <68115056+F3INTH34RTED@users.noreply.github.com>
2021-06-24 22:15:32 +0400

    Change CPU function

--- Exponentiation/Source.c
+++ Exponentiation/Source.c
@@ -6,23 +6,34 @@
 #include <OpenCL/cl.h>
 #else
 #include <CL/cl.h>
+#define __CRT__SECURE__NO__WARNINGS
 #endif // __APPLE__
 
 #define ARRAY_SIZE 1024
 const unsigned int C_ARRAY_SIZE = ARRAY_SIZE;
+
+typedef int bool;
+enum { bool_false, bool_true };
+
 enum { FIRST_ARRAY_PART = 1, SECOND_ARRAY_PART, FULL_ARRAY_PART };
 
 char *Get_Kernel_File(char *file_name);
-void Execute_On_Cpu(int array_size_percentage, int processing_part);
-
+void Execute_On_Cpu(unsigned int array_size_percentage, int processing_part);
+int Array_Part_Percentage_Valid(unsigned int array_size_percentage, int processing_part);
+int Array_Part_Percentage_Matches(unsigned int *array_size_percentage, int *processing_part);
 
 int main()
 {
-    Execute_On_Cpu(50, FIRST_ARRAY_PART);
+    Execute_On_Cpu(100, FIRST_ARRAY_PART);
 	system("pause");
 	return 0;
 }
 
+/*
+* Usage: char *filename = Get_Kernel_File("filename")
+* -----------------------------------------------------------------------------
+* Description: This method takes the filename as input parameter and returns the content of the filename if it exists.
+*/
 char *Get_Kernel_File(char *file_name)
 {
     FILE *programFile;
@@ -42,34 +53,63 @@ char *Get_Kernel_File(char *file_name)
 }
 
 /*
-* Usage: Execute_On_Cpu(int 0-100, int 1-3)
-* ------------------------------------------
-* Description: This method takes the array size in percentage (1 - 100) as the first parameter and the part of array to execute on
-first part, second part, or full part of the array. The first part of the array will execute the first array_size_percentage elements of the array, the second
-part of the array will execute second array_size_percentage elements of the array, and full part will execute the whole array
+* Usage: int boolVariable = Array_Part_Percentage_Valid(int 0-100, int 1-3)
+* ---------------------------------------------------------------------------
+* Description: Checks to see whether the array percentage and processing part is within the range 0 - 100 or 1 -3 respectively.
+Returns false if condition fails and true otherwise.
 */
-void Execute_On_Cpu(int array_size_percentage, int processing_part)
+int Array_Part_Percentage_Valid(unsigned int array_size_percentage, int processing_part)
 {
-    if (array_size_percentage != 100 && processing_part == FULL_ARRAY_PART)
+    if (array_size_percentage > 100 || array_size_percentage < 0)
     {
-        printf("Array size percentage is not 100, cannot process full array.\n");
-        printf("Would you like to continue processing the whole array (Y/N)?\n");
-        char choice = '\0';
-        scanf("%c", &choice);
-        if (choice == 'n' || choice == 'N') return;
-        else array_size_percentage = 100;
-    }
-    if (array_size_percentage == 100 && processing_part != FULL_ARRAY_PART)
-    {
-        printf("Processing part of the array chosen is not full, cannot process full array.\n");
-        printf("Would you like to continue processing the whole array (Y/N)?\n");
-        char choice = '\0';
-        scanf("%c", &choice);
-        if (choice == 'n' || choice == 'N') return;
-        else processing_part = FULL_ARRAY_PART;
-        return;
+        printf("Error: Array size percentage not within the range 0 - 100.\n");
+        return bool_false;
     }
 
-    double a_s = array_size_percentage / 100.0;
-    printf("%lf\n", a_s);
+    if (processing_part > FULL_ARRAY_PART || processing_part < FIRST_ARRAY_PART)
+    {
+        printf("Error: Processing part not within the range 1 - 3.\n");
+        return bool_false;
+    }
+
+    return bool_true;
+}
+
+/*
+* Usage: int boolVariable = Array_Part_Percentage_Matches(int 0-100, int 1-3)
+* -----------------------------------------------------------------------------
+* Description: Checks to see whether array percentage and processing part is 100 and full array part respectively. Returns false if condition fails and true
+otherwise.
+*/
+int Array_Part_Percentage_Matches(unsigned int *array_size_percentage, int *processing_part)
+{
+    if (*array_size_percentage != 100 && *processing_part == FULL_ARRAY_PART)
+    {
+        printf("Error: Array size percentage is not 100, cannot process full array.\n");
+        printf("Would you like to continue processing the whole array (Y/N)?\n");
+        char choice = scanf("%c", &choice);
+        if (choice == 'n' || choice == 'N') return bool_false;
+        *array_size_percentage = 100;
+    }
+
+    if (*array_size_percentage == 100 && *processing_part != FULL_ARRAY_PART)
+    {
+        printf("Error: Processing part of the array chosen is not full, cannot process full array.\n");
+        printf("Would you like to continue processing the whole array (Y/N)?\n");
+        char choice = scanf("%c", &choice);
+        if (choice == 'n' || choice == 'N') return bool_false;
+        *processing_part = FULL_ARRAY_PART;
+    }
+    return bool_true;
+}
+
+/*
+* Usage: Execute_On_Cpu(int 0-100, int 1-3)
+* ------------------------------------------
+* Description: Executes first, second, or full part based on the percentage of elements of an array that is to be executed on the cpu.
+*/
+void Execute_On_Cpu(unsigned int array_size_percentage, int processing_part)
+{
+    if (!Array_Part_Percentage_Valid) return;
+    printf("%d\n%d\n", array_size_percentage, processing_part);
 }
